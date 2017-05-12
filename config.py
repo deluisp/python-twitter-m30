29b7b94028b3d02460b865e75e8d699f9088f4c1
Pablo de Luis <deluisp@gmail.com>
2017-05-12 19:04:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-consumer_key =  "iXgVJo7GVX61DmeIxUfKj6d11"
-consumer_secret =  "Zhi0DYa1uKOwDmYmfl8F5MkBmrGT4PPIOMBcBsSz2349tjhtli"
-access_token =  "854698809124892675-ZYsg9QXsIoTj4fEPt1rINurByCT18G7"
-access_token_secret = "fhw67spumt8I3lqKK7DjVCpoIBITL2UvWshgv10pTwTBI"
+consumer_key =  ""
+consumer_secret =  ""
+access_token =  ""
+access_token_secret = ""
